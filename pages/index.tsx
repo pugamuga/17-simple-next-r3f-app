867599d14dd6fc8a90b06f0ecfd313b7df69af7d
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-11-06 13:26:39 +0200

    add simple loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
+import { Center, Html } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import MainScene from "../components/MainScene";
 
 const Home: NextPage = (): JSX.Element => {
   const [userWidth, setUserWidth] = useState<number>(0);
-
- 
 
   useEffect(() => {
     const resizeHandler = () => {
@@ -23,7 +22,19 @@ const Home: NextPage = (): JSX.Element => {
   return (
     <div className="h-screen md:w-full ">
       <Canvas>
-        <MainScene scale={userWidth>600?1:.6} />
+        <Suspense
+          fallback={
+            <Center>
+              <Html>
+                <p className=" text-white font-extrabold text-5xl w-[400px] text-center">
+                  Loading model...
+                </p>
+              </Html>
+            </Center>
+          }
+        >
+          <MainScene scale={userWidth > 600 ? 1 : 0.6} />
+        </Suspense>
       </Canvas>
     </div>
   );
