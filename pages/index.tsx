e50aa3225b14ebe4f45611182288e04d3547a31b
Evgeny Pugachev <rabota.pugamuga@gmail.com>
2022-11-06 13:19:54 +0200

    add resizing for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,29 @@
 import { Canvas } from "@react-three/fiber";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import MainScene from "../components/MainScene";
 
 const Home: NextPage = (): JSX.Element => {
+  const [userWidth, setUserWidth] = useState<number>(0);
+
+ 
+
+  useEffect(() => {
+    const resizeHandler = () => {
+      setUserWidth(window.innerWidth);
+    };
+    setUserWidth(window.innerWidth);
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  }, [userWidth]);
+
   return (
-    <div className="h-screen">
+    <div className="h-screen md:w-full ">
       <Canvas>
-        <MainScene />
+        <MainScene scale={userWidth>600?1:.6} />
       </Canvas>
     </div>
   );
